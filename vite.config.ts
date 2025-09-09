61571ed567b3812a62049f7f505b5839e1c70342
DatAi club <dataiclub.ensah@gmail.com>
2025-09-09 16:00:56 +0100

    'fix'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "msn-tagger";
+import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   base: process.env.VITE_BASE_PATH || "/portfolio",
